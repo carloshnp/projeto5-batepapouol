d0084b4be3f3e257dd2986ddef14fd0ab423007c
carloshnp <carloshnpwork@gmail.com>
2022-08-28 17:33:58 -0300

    recebe as mensagens e exibe no chat

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let messages;
+
 const user = {name: prompt('Qual é o seu nome?')};
 const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user);
 
@@ -8,6 +10,7 @@ function sucesso(resposta) {
     const status = resposta.status;
     console.log(status);
     statusUser();
+    getMessages();
 }
 
 function erro(resposta) {
@@ -32,5 +35,54 @@ function statusUser() {
     function offline (resposta) {
         const status = resposta.response.status;
         console.log(status);
+    }
+}
+
+function getMessages() {
+    let dados = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    
+    dados.then(data);
+
+    function data(response) {
+        const message = document.querySelector('.content');
+        message.innerHTML = '';
+        messages = response.data;
+        messages.forEach(element => renderMessages(element));
+        message.lastElementChild.scrollIntoView();
+        console.log(messages);
+    }
+}
+
+function renderMessages(element) {
+    const message = document.querySelector('.content');
+
+    if (element.type == 'message') {
+        message.innerHTML += `
+        <div class='message'>
+            <span>${element.time}</span>
+            <span>${element.from}</span>
+            <span>${element.to}</span>
+            <span>${element.text}</span>
+        </div> `
+    }
+
+    if (element.type == 'status') {
+        message.innerHTML += `
+        <div class='message'>
+            <span>${element.time}</span>
+            <span>${element.from}</span>
+            <span>${element.to}</span>
+            <span>${element.text}</span>
+        </div> `
+    }
+
+    if (element.type == 'private_message') {
+        message.innerHTML += `
+        <div class='message'>
+            <span>${element.time}</span>
+            <span>${element.from}</span>
+            <span>${element.to}</span>
+            <span>${element.text}</span>
+        </div> `
     }
 }
