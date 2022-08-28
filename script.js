6bf73dc195dbfac08d0adc05a1a43567a46cacf7
carloshnp <carloshnpwork@gmail.com>
2022-08-28 20:19:36 -0300

    mensagens e estilos implementados

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 let messages;
+let user;
 
-const user = {name: prompt('Qual é o seu nome?')};
-const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user);
-
-request.then(sucesso);
-request.catch(erro);
-
-function sucesso(resposta) {
-    const status = resposta.status;
-    console.log(status);
-    statusUser();
-    getMessages();
+function userName() {
+    user = {name: prompt('Qual é o seu nome?')};
+    const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user);
+    
+    request.then(sucesso);
+    request.catch(erro);
+    
+    function sucesso(resposta) {
+        const status = resposta.status;
+        console.log(status);
+        statusUser();
+        getMessages();
+    }
+    
+    function erro(resposta) {
+        const status = resposta.response.status;
+        console.log(status);
+    }
 }
 
-function erro(resposta) {
-    const status = resposta.response.status;
-    console.log(status);
-}
+userName();
+
 
 function statusUser() {
 
@@ -39,18 +45,19 @@ function statusUser() {
 }
 
 function getMessages() {
-    let dados = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    
-    dados.then(data);
+    setInterval(() => {
+        let dados = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
 
-    function data(response) {
-        const message = document.querySelector('.content');
-        message.innerHTML = '';
-        messages = response.data;
-        messages.forEach(element => renderMessages(element));
-        message.lastElementChild.scrollIntoView();
-        console.log(messages);
-    }
+        dados.then(data);
+
+        function data(response) {
+            const message = document.querySelector('.content');
+            message.innerHTML = '';
+            messages = response.data;
+            messages.forEach(element => renderMessages(element));
+            message.lastElementChild.scrollIntoView();
+        }
+    }, 3000);
 }
 
 function renderMessages(element) {
@@ -59,30 +66,21 @@ function renderMessages(element) {
     if (element.type == 'message') {
         message.innerHTML += `
         <div class='message'>
-            <span>${element.time}</span>
-            <span>${element.from}</span>
-            <span>${element.to}</span>
-            <span>${element.text}</span>
+            <p> <span class='time'>(${element.time})</span> <span class='from'>${element.from}</span>para <span class='to'>${element.to}</span>: <span class='text'>${element.text}</span> </p>
         </div> `
     }
 
     if (element.type == 'status') {
         message.innerHTML += `
-        <div class='message'>
-            <span>${element.time}</span>
-            <span>${element.from}</span>
-            <span>${element.to}</span>
-            <span>${element.text}</span>
+        <div class='message status'>
+            <p> <span class='time'>(${element.time})</span> <span class='from'>${element.from}</span><span class='text'>${element.text}</span> </p>
         </div> `
     }
 
-    if (element.type == 'private_message') {
+    if (element.type == 'private_message' && element.to == user) {
         message.innerHTML += `
-        <div class='message'>
-            <span>${element.time}</span>
-            <span>${element.from}</span>
-            <span>${element.to}</span>
-            <span>${element.text}</span>
+        <div class='message private_message'>
+           <p> <span class='time'>(${element.time})</span> <span class='from'>${element.from}</span> reservadamente para <span class='to'>${element.to}</span>: <span class='text'>${element.text}</span> </p>
         </div> `
     }
 }
