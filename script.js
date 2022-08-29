faf206849237fb22c06db4d8fd4750bd726e95b7
carloshnp <carloshnpwork@gmail.com>
2022-08-28 22:08:00 -0300

    mensagens e layout implementados. projeto finalizado (sem bônus)

--- script.js
+++ script.js
@@ -11,13 +11,15 @@ function userName() {
     function sucesso(resposta) {
         const status = resposta.status;
         console.log(status);
-        statusUser();
+        setInterval(() => { statusUser();}, 5000);
         getMessages();
     }
     
     function erro(resposta) {
         const status = resposta.response.status;
         console.log(status);
+        alert('Este nome já está em uso! Digite outro nome.');
+        userName();
     }
 }
 
@@ -28,11 +30,9 @@ function statusUser() {
 
     const userStatus = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', user);
 
-    setInterval(() => {
-        userStatus.then(online);
-        userStatus.catch(offline);
-    }, 5000);
-
+    userStatus.then(online);
+    userStatus.catch(offline);
+    
     function online(resposta) {
         const status = resposta.status;
         console.log(status);
@@ -45,20 +45,20 @@ function statusUser() {
 }
 
 function getMessages() {
-    setInterval(() => {
-        let dados = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    let dados = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
 
-        dados.then(data);
+    dados.then(data);
 
-        function data(response) {
-            const message = document.querySelector('.content');
-            message.innerHTML = '';
-            messages = response.data;
-            messages.forEach(element => renderMessages(element));
-            message.lastElementChild.scrollIntoView();
-        }
-    }, 3000);
+    function data(response) {
+        const message = document.querySelector('.content');
+        message.innerHTML = '';
+        messages = response.data;
+        messages.forEach(element => renderMessages(element));
+        message.lastElementChild.scrollIntoView();
+    }
 }
+
+setInterval(() => {getMessages();}, 3000);
 
 function renderMessages(element) {
     const message = document.querySelector('.content');
@@ -77,10 +77,45 @@ function renderMessages(element) {
         </div> `
     }
 
-    if (element.type == 'private_message' && element.to == user) {
+    if (element.type == 'private_message' && element.to == Object.values(user)[0]) {
         message.innerHTML += `
         <div class='message private_message'>
            <p> <span class='time'>(${element.time})</span> <span class='from'>${element.from}</span> reservadamente para <span class='to'>${element.to}</span>: <span class='text'>${element.text}</span> </p>
         </div> `
+    }
+}
+
+function sendMessage() {
+    const element = document.querySelector('.envio');
+    let inputMsg = element.value;
+    console.log(inputMsg);
+    if (inputMsg !== '') {
+        messageInput = {
+            from: Object.values(user)[0],
+	        to: "Todos",
+            text: `${element.value}`,
+            type: "message"
+        }
+    }
+    console.log(inputMsg);
+    console.log(messageInput);
+    
+    element.value = '';
+
+    const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', messageInput);
+    
+    request.then(envio);
+    request.catch(erro);
+
+    function envio(resposta) {
+        getMessages();
+        const status = resposta.status;
+        console.log(status);
+    }
+
+    function erro(resposta) {
+        const status = resposta.response.status;
+        console.log(status);
+        window.location.reload();
     }
 }
